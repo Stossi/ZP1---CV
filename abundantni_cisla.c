0cff8cbd90d58b5e51e6b60a47acc1a9d4ab5b8f
Stossi <stos.jakub@gmail.com>
2013-11-20 16:41:09 +0100

    Update abundantni_cisla.c

--- abundantni_cisla.c
+++ abundantni_cisla.c
@@ -1,45 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int abundantni_cisla(int limit)
 {
-    int i, j = 28123, soucet, poc=0;
+    int i, j, soucet, poc=0;
     int* pole = (int*)malloc(sizeof(int) * limit);
 
-    for (j = 100000; j > 0; j--)
+    //projde vsechna cisla
+    for (j = 1; j < limit; j++)
         {
             soucet = 0;
 
-            for (i = 2; i < j; i++)
+            for (i = 2; i <= sqrt(j); i++)
             {
-                if (j % i == 0)
+                if ((j % i == 0)&&(i < (j/i)))
                 {
-                    //pole[i] = i;
-                    soucet += i;//pole[i];
+                    if(i < j/i)
+                        {
+                            soucet += j/i;
+                        }
+                    soucet += i;
                 }
             }
             if (soucet > j)
             {
                     poc++;
                     pole[poc-1] = j;
-                    //printf("%i ", pole[poc]);
             }
         }
+        //return poc;
+        //vypis pole
         for (i = 0; i < poc; i++)
             {
-                /*for(j = i; j < poc; j++)
-                    {
-                        if (pole[i] + pole[j] <= limit)
-                            {
-                                printf ("%i ",pole[i]);
-                            }
-                    }*/
                 printf ("%i ",pole[i]);
             }
-
 }
 int main()
 {
-    abundantni_cisla(100000);
+    //printf("%i \n",abundantni_cisla(100000));
+    abundantni_cisla(1000000);
     return 0;
 }
